09f8526ee278fa9ccbe7a7bcaeed8adf6685764d
Luis Arce <luis.arce22@gmail.com>
2019-09-13 22:55:24 -0400

    feat(db): query data

--- db.go
+++ db.go
@@ -3,13 +3,24 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	fmt.Println("Go Mysql Test")
+type Product struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
+func main() {
+
+	queryDb()
+
+}
+
+func queryDb() {
+	fmt.Println("Go Mysql Query Test")
 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/store")
 
 	if err != nil {
@@ -18,6 +29,35 @@ func main() {
 
 	defer db.Close()
 
+	results, err := db.Query("Select id, name From products")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var product Product
+
+		err = results.Scan(&product.ID, &product.Name)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		log.Printf(product.Name)
+	}
+
+}
+
+func insertRow() {
+	fmt.Println("Go Mysql Insert Test")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/store")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
 	insert, err := db.Query("INSERT INTO products(name, price) VALUES ('Product 1', '10')")
 
 	if err != nil {
@@ -25,5 +65,4 @@ func main() {
 	}
 
 	defer insert.Close()
-
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module example.com/hello
 go 1.12
 
 require (
-	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/go-sql-driver/mysql v1.4.1
 	rsc.io/quote/v3 v3.1.0
 )
